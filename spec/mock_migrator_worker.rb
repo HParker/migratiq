2134160789470d97c845f15ca97d1b7249e09236
Adam Hess <adamhess1991@gmail.com>
2015-10-15 11:39:24 -0600

    add migrator worker

---
+++ spec/mock_migrator_worker.rb
@@ -0,0 +1,15 @@
+class MockMigratorWorker
+  include Sidekiq::Worker
+  include Migratiq
+
+
+
+  def perform(a, b, c = 1)
+    puts "Doin work!"
+  end
+
+
+  migrate_by(arity: 5) do |a, b, c, d, e|
+    [b, a]
+  end
+end
