59d76e4a875ca158dbb6f163db70c57302e7c4df
Adam Hess <adamhess1991@gmail.com>
2015-12-02 22:41:04 -0700

    add frozen string magic comment at the top of all files

--- lib/migratiq.rb
+++ lib/migratiq.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require "migratiq/version"
 require "arity_checker"
 require 'sidekiq/api'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 $LOAD_PATH.unshift(File.expand_path('../lib', File.dirname(__FILE__)))
 require 'migratiq'
 require 'mock_worker'
